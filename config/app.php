e48aea54742701b090e248167b85179b61842bb7
Pankaj Adhyapak <pankaj@Pankajs-MacBook-Air.local>
2016-10-23 21:15:18 +0530

    Changed Style's

--- config/app.php
+++ config/app.php
@@ -78,7 +78,7 @@ return [
 	|
 	*/
 
-	'key' => env('APP_KEY', 'SomeRandomString'),
+	'key' => env('APP_KEY', 'vVKbEwuxVsnM85y9ufyb94rpp0uK9sQc'),
 
 	'cipher' => MCRYPT_RIJNDAEL_128,
 
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
 <div id="home">
 	<div class="jumbotron">
 		<div class="container">
-			 <h1>Welcome to <span id="logo-color">&lt MSRIT Code Eval /&gt;	</span></h1>
+			 <h1>Welcome to <span id="logo-color">&lt GIT Code Eval /&gt;	</span></h1>
 	 <h2> <span id="logo-color">Bring out the best developer in you.</span></h2>
 	 <br/>
 	 <br/>
@@ -12,7 +12,7 @@
   <a  href="{{ url('/auth/register') }}" class="btn btn-primary btn-lg btn-home">Log in</a>
 </p>
 		</div>
-<p> A product by MSRIT MCA Student</p>
+<p> A product by GIT MCA Student</p>
 </div>
 </div>
 
